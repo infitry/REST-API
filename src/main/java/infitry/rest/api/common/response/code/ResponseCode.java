d94cf0489b3741fd69603fece007a73dc4c592be
infitry <leesw504@gmail.com>
2022-07-27 21:37:17 +0900

    [수정] @FieldDefaults 추가

--- src/main/java/infitry/rest/api/common/response/code/ResponseCode.java
+++ src/main/java/infitry/rest/api/common/response/code/ResponseCode.java
@@ -1,11 +1,15 @@
 package infitry.rest.api.common.response.code;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public enum ResponseCode {
     OK(200),
     SERVER_ERROR(500),
     FORBIDDEN(403);
 
-    private int statusCode;
+    int statusCode;
     ResponseCode(int statusCode) {
         this.statusCode = statusCode;
     }
