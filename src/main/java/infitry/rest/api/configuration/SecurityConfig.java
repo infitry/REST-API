d7e9d3c28463c9a577e81bf1f44e856db020af32
infitry <leesw504@gmail.com>
2022-07-27 20:38:44 +0900

    [추가] Security 설정 추가

--- src/main/java/infitry/rest/api/configuration/SecurityConfig.java
+++ src/main/java/infitry/rest/api/configuration/SecurityConfig.java
@@ -1,32 +1,73 @@
 package infitry.rest.api.configuration;
 
+import infitry.rest.api.security.CustomAccessDeniedHander;
+import infitry.rest.api.security.CustomAuthenticationEntryPoint;
+import infitry.rest.api.security.jwt.JWTFilter;
+import infitry.rest.api.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
-
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    private final CustomAccessDeniedHander customAccessDeniedHander;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final TokenProvider tokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.cors().and()
-//                .csrf().disable().authorizeHttpRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .httpBasic().disable()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
+        http.csrf().disable()
+                .exceptionHandling()
+                    .accessDeniedHandler(customAccessDeniedHander)
+                    .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                    .cors()
+                .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .formLogin().disable().headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/api/v1/authentication").permitAll()
+                .and()
+                    .addFilterBefore(new JWTFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", configuration);
+        return configurationSource;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/static/**");
     }
 }
