1d2c6372046c26a98a7c1be8c3e0b561a97e273b
infitry <leesw504@gmail.com>
2022-08-02 20:05:45 +0900

    [수정] @Component -> @Service 로 변경

--- src/main/java/infitry/rest/api/configuration/security/token/TokenProvider.java
+++ src/main/java/infitry/rest/api/configuration/security/token/TokenProvider.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
@@ -22,7 +23,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
+@Service
 @Transactional(readOnly = true)
 public class TokenProvider {
     public static final String AUTHORITIES_KEY = "auth";
