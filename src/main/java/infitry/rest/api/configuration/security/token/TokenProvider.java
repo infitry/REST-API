2914840cc2a7af8a1655296c475faecccc66e4b5
infitry <leesw504@gmail.com>
2022-08-04 19:56:55 +0900

    [수정] 불 필요한 임포트 제거

--- src/main/java/infitry/rest/api/configuration/security/token/TokenProvider.java
+++ src/main/java/infitry/rest/api/configuration/security/token/TokenProvider.java
@@ -13,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
