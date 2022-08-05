fbc272357f347fd4638930a0d7ad0f399c70d655
infitry <leesw504@gmail.com>
2022-08-05 12:44:21 +0900

    [추가] TestConfiguration 추가

---
+++ src/test/java/infitry/rest/api/configuration/TestJPAConfig.java
@@ -0,0 +1,9 @@
+package infitry.rest.api.configuration;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@TestConfiguration
+public class TestJPAConfig {
+}
