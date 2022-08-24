2821da25196269d2a0adb6f477dc34233c06d51b
infitry <leesw504@gmail.com>
2022-08-24 14:33:24 +0900

    [수정] 인터페이스 멤버변수 기본 속성에 맞춰 수정

--- src/main/java/infitry/rest/api/common/constant/UserConstant.java
+++ src/main/java/infitry/rest/api/common/constant/UserConstant.java
@@ -1,6 +1,6 @@
 package infitry.rest.api.common.constant;
 
 public interface UserConstant {
-    public static final long ROLE_USER_ID = 1L;
-    public static final long ROLE_ADMIN_ID = 2L;
+    long ROLE_USER_ID = 1L;
+    long ROLE_ADMIN_ID = 2L;
 }
