6d4b13fc4caab8475132b103e3f11698d0ac3d0b
infitry <leesw504@gmail.com>
2022-09-26 20:55:27 +0900

    [수정] 파일 API 설계

--- src/main/java/infitry/rest/api/controller/file/FileController.java
+++ src/main/java/infitry/rest/api/controller/file/FileController.java
@@ -5,10 +5,10 @@ import infitry.rest.api.dto.file.FileUploadRequest;
 import infitry.rest.api.util.ResponseUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 @Tag(name = "Files", description = "파일 업,다운로드 컨트롤러")
 @RestController
@@ -17,8 +17,15 @@ public class FileController {
 
     @Operation(summary = "파일 업로드", description = "파일을 업로드 하는 공통 API")
     @PostMapping("/upload")
-    public CommonResponse fileUpload(FileUploadRequest fileUploadRequest) {
+    public CommonResponse fileUpload(@Valid @RequestBody FileUploadRequest fileUploadRequest) {
         fileUploadRequest.getMultipartFiles().stream().map(MultipartFile::getName).forEach(System.out::println);
+        return ResponseUtil.successResponse();
+    }
+
+    @Operation(summary = "파일 다운로드", description = "파일을 다운로드 하는 공통 API")
+    @GetMapping("/{fileId}")
+    public CommonResponse fileDownload(@PathVariable Long fileId) {
+        System.out.println("fileId = " + fileId);
         return ResponseUtil.successResponse();
     }
 }
