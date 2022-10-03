6f72d068172a193c9c3bff6a1216547dda44e9b2
infitry <leesw504@gmail.com>
2022-10-03 23:40:49 +0900

    [수정] 파일 업로드 중간커밋

--- src/main/java/infitry/rest/api/service/file/FileService.java
+++ src/main/java/infitry/rest/api/service/file/FileService.java
@@ -1,6 +1,7 @@
 package infitry.rest.api.service.file;
 
 import infitry.rest.api.dto.file.FileDto;
+import infitry.rest.api.exception.ServiceException;
 import infitry.rest.api.repository.FileRepository;
 import infitry.rest.api.repository.domain.file.File;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -22,6 +28,7 @@ public class FileService {
 
     /** 파일 저장하기 */
     public List<FileDto> save(List<MultipartFile> multipartFiles) {
+
         List<File> files = multipartFiles.stream()
                 .map(file -> FileDto.builder()
                         .filePath(FILE_SAVE_PATH)
@@ -32,9 +39,35 @@ public class FileService {
                     .build())
                 .map(fileDto -> File.createFile(fileDto))
                 .collect(Collectors.toList());
-        
-        return fileRepository.saveAll(files).stream()
+
+        List<FileDto> saveFiles = fileRepository.saveAll(files).stream()
                 .map(file -> modelMapper.map(file, FileDto.class))
                 .collect(Collectors.toList());
+
+        Path root = Paths.get(FILE_SAVE_PATH);
+        if (!Files.exists(root)) {
+            createUploadDirectories();
+        }
+
+        multipartFiles.forEach(file -> {
+            try {
+                Files.copy(file.getInputStream(), root.resolve(file.getOriginalFilename()),
+                        StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                throw new ServiceException("Could not upload file (name: " + file.getName() + ")");
+            }
+
+        });
+
+        return saveFiles;
     }
+
+    private void createUploadDirectories() {
+        try {
+            Files.createDirectories(Paths.get(FILE_SAVE_PATH));
+        } catch (IOException e) {
+            throw new ServiceException("Could not create upload Directories!");
+        }
+    }
+
 }
