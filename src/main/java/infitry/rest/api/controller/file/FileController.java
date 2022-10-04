cedd9dc6e135fcfa67e1dbdb96cea91230981459
infitry <leesw504@gmail.com>
2022-10-04 12:01:13 +0900

    [수정] swagger 파일업로드 추가

--- src/main/java/infitry/rest/api/controller/file/FileController.java
+++ src/main/java/infitry/rest/api/controller/file/FileController.java
@@ -2,24 +2,28 @@ package infitry.rest.api.controller.file;
 
 import infitry.rest.api.common.response.CommonResponse;
 import infitry.rest.api.dto.file.FileUploadRequest;
+import infitry.rest.api.service.file.FileService;
 import infitry.rest.api.util.ResponseUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 
 @Tag(name = "Files", description = "파일 업,다운로드 컨트롤러")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/files")
 public class FileController {
 
+    private final FileService fileService;
+
     @Operation(summary = "파일 업로드", description = "파일을 업로드 하는 공통 API")
-    @PostMapping("/upload")
-    public CommonResponse fileUpload(@Valid @RequestBody FileUploadRequest fileUploadRequest) {
-        fileUploadRequest.getMultipartFiles().stream().map(MultipartFile::getName).forEach(System.out::println);
-        return ResponseUtil.successResponse();
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public CommonResponse fileUpload(@Valid FileUploadRequest fileUploadRequest) {
+        return ResponseUtil.getListResult(fileService.save(fileUploadRequest.getMultipartFiles()));
     }
 
     @Operation(summary = "파일 다운로드", description = "파일을 다운로드 하는 공통 API")
