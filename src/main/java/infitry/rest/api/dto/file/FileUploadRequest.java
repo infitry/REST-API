2356eb1507494691bd7981900724811e006ab2c7
infitry <leesw504@gmail.com>
2022-10-06 10:41:48 +0900

    [수정] validation 제거

--- src/main/java/infitry/rest/api/dto/file/FileUploadRequest.java
+++ src/main/java/infitry/rest/api/dto/file/FileUploadRequest.java
@@ -5,7 +5,6 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Schema(name = "FileRequest", description = "파일 업로드 요청")
@@ -17,7 +16,6 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class FileUploadRequest {
 
-    @NotBlank(message = "업로드할 파일이 존재하지 않습니다.")
     @Schema(name = "multipartFiles", description = "업로드 파일들", type = "file", example = " ")
     List<MultipartFile> multipartFiles; // 업로드할 파일들
 }
