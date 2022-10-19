e7950e8241ea3b5a59ce596396493ccf27b5f8a4
infitry <leesw504@gmail.com>
2022-10-19 11:43:53 +0900

    [수정] 키 값 추가

--- src/main/java/infitry/rest/api/common/constant/UserConstant.java
+++ src/main/java/infitry/rest/api/common/constant/UserConstant.java
@@ -3,4 +3,9 @@ package infitry.rest.api.common.constant;
 public interface UserConstant {
     long ROLE_USER_ID = 1L;
     long ROLE_ADMIN_ID = 2L;
+    String AUTHORITIES_KEY = "authorities";
+    String NAME_KEY = "name";
+    String EMAIL_KEY = "email";
+    String PHONE_NUMBER_KEY = "phoneNumber";
+    String AUTHORITIES_SEPARATOR = ",";
 }
