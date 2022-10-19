63667bcb202e12a88c9283506ff55350279bd3b4
infitry <leesw504@gmail.com>
2022-10-19 11:44:47 +0900

    [수정] 토큰 인증 시 토큰의 클레임으로만 시큐리티 인증하도록 수정

--- src/main/java/infitry/rest/api/configuration/security/token/TokenProvider.java
+++ src/main/java/infitry/rest/api/configuration/security/token/TokenProvider.java
@@ -1,12 +1,15 @@
 package infitry.rest.api.configuration.security.token;
 
+import infitry.rest.api.common.constant.UserConstant;
 import infitry.rest.api.common.response.code.ResponseCode;
+import infitry.rest.api.configuration.aop.timer.Timer;
 import infitry.rest.api.dto.token.TokenDto;
 import infitry.rest.api.dto.user.AuthorityDto;
 import infitry.rest.api.dto.user.UserDto;
 import infitry.rest.api.exception.ServiceException;
 import infitry.rest.api.repository.UserRepository;
 import infitry.rest.api.repository.domain.user.User;
+import infitry.rest.api.repository.domain.user.code.Role;
 import infitry.rest.api.service.redis.RedisService;
 import infitry.rest.api.util.DateUtil;
 import io.jsonwebtoken.*;
@@ -15,10 +18,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -27,7 +33,6 @@ import java.util.stream.Collectors;
 @Service
 @Transactional(readOnly = true)
 public class TokenProvider {
-    public static final String AUTHORITIES_KEY = "auth";
     public static final String SIGNING_KEY = "infitrysignkey";
     public static final String REDIS_GROUP_PREFIX = "refresh-token:";
     private static final Long HOURS_ACCESS_TOKEN_EXPIRE = 1L;
@@ -36,6 +41,7 @@ public class TokenProvider {
     private final RedisService redisService;
     private final UserRepository userRepository;
 
+    /** 엑세스, 리프레쉬 토큰을 발급한다. */
     @Transactional
     public TokenDto generateToken(UserDto userDto) {
         String refreshToken = generateRefreshToken();
@@ -46,30 +52,27 @@ public class TokenProvider {
                 .refreshToken(refreshToken)
                 .build();
     }
+    /** 엑세스 토큰을 발급한다. */
     public String generateAccessToken(UserDto userDto) {
-
         return Jwts.builder()
                 .setSubject(userDto.getId())
-                .claim(AUTHORITIES_KEY, getAuthorities(userDto))
-                .claim("name", userDto.getName())
-                .claim("phoneNumber", userDto.getPhoneNumber())
-                .claim("email", userDto.getEmail())
+                .claim(UserConstant.AUTHORITIES_KEY, convertStringAuthorities(userDto))
+                .claim(UserConstant.NAME_KEY, userDto.getName())
+                .claim(UserConstant.PHONE_NUMBER_KEY, userDto.getPhoneNumber())
+                .claim(UserConstant.EMAIL_KEY, userDto.getEmail())
                 .setIssuedAt(new Date())
                 .signWith(SignatureAlgorithm.HS512, SIGNING_KEY)
                 .setExpiration(DateUtil.addHours(HOURS_ACCESS_TOKEN_EXPIRE))
                 .compact();
     }
 
-    private User getUser(Authentication authentication) {
-        return (User) authentication.getPrincipal();
-    }
-
-    private String getAuthorities(UserDto userDto) {
+    /** 사용자의 권한들을 ","를 구분자로 String 으로 변환한다. */
+    private String convertStringAuthorities(UserDto userDto) {
         return userDto.getAuthorities().stream()
                 .map(authorityDto -> authorityDto.getRole().name())
-                .collect(Collectors.joining(","));
+                .collect(Collectors.joining(UserConstant.AUTHORITIES_SEPARATOR));
     }
-
+    /** 리프레쉬 토큰을 발급한다. */
     public String generateRefreshToken() {
         return Jwts.builder()
                 .setIssuedAt(new Date())
@@ -77,7 +80,7 @@ public class TokenProvider {
                 .setExpiration(DateUtil.addHours(HOURS_REFRESH_TOKEN_EXPIRE))
                 .compact();
     }
-
+    /** 유효한 토큰인지 확인한다. */
     public boolean isValidToken(String token) {
         try {
             Jwts.parser().setSigningKey(SIGNING_KEY).parseClaimsJws(token);
@@ -95,14 +98,21 @@ public class TokenProvider {
         }
         return false;
     }
+    /**
+     * 매 요청 Header의 Token을 파싱하여 인증정보 조회
+     * principal : UserDto
+     **/
+    @Timer
     public Authentication getAuthenticationByToken(String token) {
-        return getAuthentication(findUserById(getUserIdInClaims(parseClaims(token))));
+        return getAuthentication(makeUserByClaims(parseClaims(token)));
     }
 
+    /** 엑세스 토큰을 재발급 한다. */
     public String reissueAccessToken(String refreshToken) {
         return generateAccessToken(findUserById(getRedisValue(refreshToken)).toDto());
     }
 
+    /** 사용자 ID로 유저를 찾는다. */
     private User findUserById(String userId) {
         return userRepository.findById(userId).orElseThrow(() -> new ServiceException("사용자를 찾을 수 없습니다.", ResponseCode.UNAUTHORIZED));
     }
@@ -111,8 +121,27 @@ public class TokenProvider {
         return redisService.getValue(REDIS_GROUP_PREFIX + key, String.class);
     }
 
-    private Authentication getAuthentication(User user) {
-        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+    private Authentication getAuthentication(UserDto userDto) {
+        return new UsernamePasswordAuthenticationToken(userDto, null, convertAuthorityDtosToSecurityAuthorities(userDto.getAuthorities()));
+    }
+
+    private static List<GrantedAuthority> convertAuthorityDtosToSecurityAuthorities(List<AuthorityDto> authorityDtos) {
+        return authorityDtos.stream().map(authorityDto -> new SimpleGrantedAuthority(authorityDto.getRole().name())).collect(Collectors.toList());
+    }
+
+    private UserDto makeUserByClaims(Claims claims) {
+        return UserDto.builder()
+                .id(claims.getSubject())
+                .name(claims.get(UserConstant.NAME_KEY, String.class))
+                .email(claims.get(UserConstant.EMAIL_KEY, String.class))
+                .phoneNumber(claims.get(UserConstant.PHONE_NUMBER_KEY, String.class))
+                .authorities(convertStringAuthoritiesToAuthorityDtos(claims))
+            .build();
+    }
+
+    private List<AuthorityDto> convertStringAuthoritiesToAuthorityDtos(Claims claims) {
+       return Arrays.stream(claims.get(UserConstant.AUTHORITIES_KEY, String.class).split(UserConstant.AUTHORITIES_SEPARATOR))
+               .map(authority -> AuthorityDto.builder().role(Role.valueOf(authority)).build()).collect(Collectors.toList());
     }
 
     private Claims parseClaims(String token) {
@@ -120,9 +149,5 @@ public class TokenProvider {
                 .setSigningKey(SIGNING_KEY)
                 .parseClaimsJws(token)
                 .getBody();
-    }
-
-    private String getUserIdInClaims(Claims claims) {
-        return claims.getSubject();
     }
 }
--- src/test/java/infitry/rest/api/configuration/security/token/TokenProviderTest.java
+++ src/test/java/infitry/rest/api/configuration/security/token/TokenProviderTest.java
@@ -17,7 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TokenProviderTest {
@@ -34,8 +34,9 @@ class TokenProviderTest {
         // given
         final String encodedPassword = passwordEncoder.encode("password");
         Authority authority = Authority.createAuthority(Role.ROLE_ADMIN, "어드민 기능 허용");
+        Authority authority2 = Authority.createAuthority(Role.ROLE_USER, "사용자 기능 허용");
         AddressDto addressDto = AddressDto.builder().zipCode("111-111").address("서울시 마포구").addressDetail("성산동 111").build();
-        User user = User.createUser(List.of(authority), UserDto.builder().id("user1").name("회원1").password(encodedPassword).addressDto(addressDto).build());
+        User user = User.createUser(List.of(authority, authority2), UserDto.builder().id("user1").name("회원1").phoneNumber("01000000000").email("test@testtt.com").password(encodedPassword).addressDto(addressDto).build());
         authentication = new UsernamePasswordAuthenticationToken(user, encodedPassword, List.of(new SimpleGrantedAuthority(authority.getRole().name())));
     }
 
@@ -52,5 +53,20 @@ class TokenProviderTest {
         //then
         assertTrue(tokenProvider.isValidToken(tokenDto.getAccessToken()));
         assertTrue(tokenProvider.isValidToken(tokenDto.getRefreshToken()));
+    }
+
+    @Test
+    public void 토큰으로_사용자_인증조회() {
+        // given
+        UserDto userDto = ((User) authentication.getPrincipal()).toDto();
+        TokenDto tokenDto = tokenProvider.generateToken(userDto);
+        // when
+        Authentication auth = tokenProvider.getAuthenticationByToken(tokenDto.getAccessToken());
+        UserDto tokenUserDto = (UserDto) auth.getPrincipal();
+        // then
+        assertEquals(userDto.getId(), tokenUserDto.getId(), "기존 아이디와 토큰 파싱 후 아이디가 같아야 한다.");
+        assertEquals(userDto.getName(), tokenUserDto.getName(), "기존 이름과 토큰 파싱 후 이름이 같아야 한다.");
+        assertEquals(userDto.getPhoneNumber(), tokenUserDto.getPhoneNumber(), "기존 휴대전호번호와 토큰 파싱 후 휴대전호번호이 같아야 한다.");
+        assertEquals(userDto.getEmail(), tokenUserDto.getEmail(), "기존 이메일과 토큰 파싱 후 이메일이 같아야 한다.");
     }
 }
