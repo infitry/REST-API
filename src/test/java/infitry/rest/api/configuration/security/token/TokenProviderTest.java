ca941bab8070305825228c68a267f79025a3fc4b
infitry <leesw504@gmail.com>
2022-10-22 18:35:30 +0900

    [수정] 사용자 생성 방법 변경 엑세스 토큰 재발급 테스트 추가

--- src/test/java/infitry/rest/api/configuration/security/token/TokenProviderTest.java
+++ src/test/java/infitry/rest/api/configuration/security/token/TokenProviderTest.java
@@ -1,50 +1,36 @@
 package infitry.rest.api.configuration.security.token;
 
 import infitry.rest.api.dto.token.TokenDto;
-import infitry.rest.api.dto.user.AddressDto;
 import infitry.rest.api.dto.user.UserDto;
-import infitry.rest.api.repository.AuthorityRepository;
 import infitry.rest.api.repository.UserRepository;
-import infitry.rest.api.repository.domain.user.Authority;
 import infitry.rest.api.repository.domain.user.User;
-import infitry.rest.api.repository.domain.user.code.Role;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Transactional(readOnly = true)
 class TokenProviderTest {
-    @Autowired
-    AuthorityRepository authorityrepository;
     @Autowired
     UserRepository userRepository;
     @Autowired
     TokenProvider tokenProvider;
-    @Autowired
-    PasswordEncoder passwordEncoder;
     Authentication authentication;
 
     @BeforeEach
     void 사용자_생성() {
         // given
-        final String encodedPassword = passwordEncoder.encode("password");
-        Authority authority = Authority.createAuthority(Role.ROLE_ADMIN, "어드민 기능 허용");
-        authorityrepository.save(authority);
-        Authority authority2 = Authority.createAuthority(Role.ROLE_USER, "사용자 기능 허용");
-        authorityrepository.save(authority2);
-        AddressDto addressDto = AddressDto.builder().zipCode("111-111").address("서울시 마포구").addressDetail("성산동 111").build();
-        User user = User.createUser(List.of(authority, authority2), UserDto.builder().id("user1").name("회원1").phoneNumber("01000000000").email("test@testtt.com").password(encodedPassword).addressDto(addressDto).build());
-        userRepository.save(user);
-        authentication = new UsernamePasswordAuthenticationToken(user, encodedPassword, List.of(new SimpleGrantedAuthority(authority.getRole().name())));
+        User user = userRepository.findById(1L).orElseThrow(NoSuchElementException::new);
+        authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
     }
 
     @Test
@@ -54,7 +40,6 @@ class TokenProviderTest {
 
         //when
         TokenDto tokenDto = tokenProvider.generateToken(userDto);
-
         System.out.println("tokenDto = " + tokenDto);
 
         //then
@@ -78,6 +63,7 @@ class TokenProviderTest {
     }
 
     @Test
+    @Transactional
     public void 엑세스토큰_재발급() {
         //given
         UserDto userDto = ((User) authentication.getPrincipal()).toDto();
