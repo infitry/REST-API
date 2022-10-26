86d8ab2433f15379b715ab41053d8ea0e98dd278
infitry <leesw504@gmail.com>
2022-10-26 22:44:04 +0900

    [수정] 테스트 시 Random port 제거

--- src/test/java/infitry/rest/api/service/file/FileServiceTest.java
+++ src/test/java/infitry/rest/api/service/file/FileServiceTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Transactional(readOnly = true)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class FileServiceTest {
 
     @Autowired
