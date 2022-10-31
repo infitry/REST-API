ae2908e88b7572f229b79c6cb38929b5d5c7c97c
infitry <leesw504@gmail.com>
2022-10-31 16:37:46 +0900

    [수정] 토큰 인증 필요 시 한번 인증 후 테스트 가능하도록 수정

--- src/main/java/infitry/rest/api/configuration/SwaggerConfig.java
+++ src/main/java/infitry/rest/api/configuration/SwaggerConfig.java
@@ -3,8 +3,12 @@ package infitry.rest.api.configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,12 +16,18 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .title("REST API Template")
+                .title("API")
                 .version("1.0.0")
-                .description("REST API 만들기");
+                .description("REST API");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement))
                 .info(info);
     }
 }
